ca2c14ef048042f8c962b6dc100ea3f9c4858d71
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2026-05-15 21:01:39 -0500

    chore(setup): add initial project documentation and configuration for database and SMTP

--- src/main/java/com/jcaa/usersmanagement/Main.java
+++ src/main/java/com/jcaa/usersmanagement/Main.java
@@ -1,21 +1,12 @@
 package com.jcaa.usersmanagement;
 
-import com.jcaa.usersmanagement.infrastructure.config.DependencyContainer;
-import com.jcaa.usersmanagement.infrastructure.entrypoint.desktop.cli.UserManagementCli;
-import com.jcaa.usersmanagement.infrastructure.entrypoint.desktop.cli.io.ConsoleIO;
-import java.util.Scanner;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public final class Main {
+@SpringBootApplication
+public class Main {
 
-  private static final Logger log = LoggerFactory.getLogger(Main.class);
-
-  public static void main(final String[] args) {
-    log.info("Starting Users Management System...");
-    final DependencyContainer container = new DependencyContainer();
-    try (final Scanner scanner = new Scanner(System.in)) {
-      new UserManagementCli(container.userController(), new ConsoleIO(scanner, System.out)).start();
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
   }
 }
